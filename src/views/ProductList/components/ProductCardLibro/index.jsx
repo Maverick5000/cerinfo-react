c7667b84feca261ec227ce5bfb8a6a21a6790588
Andre Krauss <andre.kmbv@gmail.com>
2019-06-28 10:01:39 -0400

    Validations

--- src/views/ProductList/components/ProductCardLibro/index.jsx
+++ src/views/ProductList/components/ProductCardLibro/index.jsx
@@ -26,36 +26,161 @@ import styles from './styles';
 import axios from 'axios';
 
 class ProductCardLibro extends Component {
-
   state = {
-    visible: true
+    visible: true,
+    userPrestamos: [],
+    userSolicitudes: []
   };
 
   deleteLibro(id) {
-    let url = 'https://cerinfo-api.herokuapp.com/libros/'
-    axios.delete(url + id)
+    let url = 'https://cerinfo-api.herokuapp.com/libros/';
+    axios
+      .delete(url + id)
       .then(res => {
-        console.log(res)
+        console.log(res);
         this.setState({ visible: false });
-      }).catch(function (error) {
-        console.log(error);
-        window.alert('No se puede eliminar este libro porque se encuentra prestado o con una multa')
       })
+      .catch(function(error) {
+        console.log(error);
+        window.alert(
+          'No se puede eliminar este libro porque se encuentra prestado o con una multa'
+        );
+      });
   }
 
   solicitarLibro(id) {
     if (window.confirm('Solicitar este libro?')) {
       const u_id = localStorage.getItem('id');
-      axios.post('https://cerinfo-api.herokuapp.com/solicituds', {
-        usuario_id: u_id,
-        libro_id: id
-      }).then(res => {
-        console.log(res)
-        window.alert('Libro Solicitado')
-      }).catch(function (error) {
-        console.log(error);
-      })
+      axios
+        .post('https://cerinfo-api.herokuapp.com/solicituds', {
+          usuario_id: u_id,
+          libro_id: id
+        })
+        .then(res => {
+          console.log(res);
+          window.alert('Libro Solicitado');
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
+  }
+
+  componentDidMount() {
+    this.getPrestamos();
+    this.getSolicitudes();
+  }
+
+  getPrestamos() {
+    const id = localStorage.getItem('id');
+    //const id = '1'
+    const tipo = localStorage.getItem('tipo_usuario');
+    let url = '';
+    if (tipo == 'Administrador') {
+      url = 'https://cerinfo-api.herokuapp.com/prestamos';
+    } else {
+      url = `https://cerinfo-api.herokuapp.com/user/prestamos`;
+    }
+
+    axios.get(url, { params: { usuario_id: id } }).then(res => {
+      this.setState({ userPrestamos: res.data });
+    });
+  }
+
+  getSolicitudes() {
+    const id = localStorage.getItem('id');
+    //const id = '1'
+    const tipo = localStorage.getItem('tipo_usuario');
+    let url = '';
+    if (tipo == 'Administrador') {
+      url = 'https://cerinfo-api.herokuapp.com/solicituds';
+    } else {
+      url = `https://cerinfo-api.herokuapp.com/user/solicituds`;
+    }
+
+    axios.get(url, { params: { usuario_id: id } }).then(res => {
+      this.setState({ userSolicitudes: res.data });
+    });
+  }
+
+  mapButton(libro_id) {
+    let button = '';
+    let flag = false;
+    let prestamos = this.state.userPrestamos;
+    for (let index = 0; index < prestamos.length; index++) {
+      const element = prestamos[index];
+      if (element.libro.id == libro_id) {
+        button = (
+          <Button
+            color='secondary'
+            size='small'
+            variant='outlined'
+            type='button'
+            style={{ marginLeft: '1vw', width: '7vw' }}>
+            Prestado
+          </Button>
+        );
+        flag = true;
+        break;
+      } else {
+        button = (
+          <Button
+            color='primary'
+            size='small'
+            variant='outlined'
+            type='button'
+            style={{ marginLeft: '1vw', width: '7vw' }}
+            onClick={() => {
+              this.solicitarLibro(libro_id);
+            }}>
+            Solicitar libro
+          </Button>
+        );
+      }
+    }
+
+    if (flag) {
+      return button;
+    } else {
+      return this.mapButtonSolicitud(libro_id);
+    }
+  }
+
+  mapButtonSolicitud(libro_id) {
+    let button = '';
+    let solicitudes = this.state.userSolicitudes;
+    for (let index = 0; index < solicitudes.length; index++) {
+      const element = solicitudes[index];
+      if (element.libro.id == libro_id) {
+        button = (
+          <Button
+            color='secondary'
+            size='small'
+            variant='outlined'
+            type='button'
+            style={{ marginLeft: '1vw', width: '7vw' }}>
+            Solicitado
+          </Button>
+        );
+        break;
+      } else {
+        button = (
+          <Button
+            color='primary'
+            size='small'
+            variant='outlined'
+            type='button'
+            style={{ marginLeft: '1vw', width: '7vw' }}
+            onClick={() => {
+              this.solicitarLibro(libro_id);
+            }}>
+            Solicitar libro
+          </Button>
+        );
+      }
+    }
+
+    return button;
   }
 
   render() {
@@ -65,67 +190,51 @@ class ProductCardLibro extends Component {
 
     return (
       <div>
-        {this.state.visible ? <Paper className={rootClassName}>
-          <div className={classes.imageWrapper}>
-            <img
-              alt="Libro"
-              className={classes.image}
-              src='https://odcspress.org/wp-content/uploads/2017/01/Stack-Books-Copy.jpg'
-            />
-          </div>
-          <div className={classes.details}>
-            <Typography
-              className={classes.title}
-              variant="h4"
-            >
-              {libro.titulo_libro}
-            </Typography>
-            <Typography
-              className={classes.description}
-              variant="body1"
-            >
-              {libro.nombre_autor}
-            </Typography>
-          </div>
-          <Divider />
-          <div className={classes.stats}>
-            {/* <AccessTimeIcon className={classes.updateIcon} /> */}
-            <Typography
-              className={classes.updateText}
-              variant="body2"
-            >
-              Genero: {libro.nombre_genero}
-            </Typography>
-            {/* <GetAppIcon className={classes.downloadsIcon} /> */}
-            <Typography
-              className={classes.downloadsText}
-              variant="body2"
-            >
-              Idioma: {libro.significado_idioma}
-            </Typography>
-            {tipo == 'Administrador' ?
-              <Button
-                color="secondary"
-                size="small"
-                variant="outlined"
-                type="button"
-                style={{ marginLeft: '1vw', width: '7vw' }}
-                onClick={() => { this.deleteLibro(libro.id) }}
-              >
-                Eliminar libro
-            </Button>
-              : <Button
-                color="primary"
-                size="small"
-                variant="outlined"
-                type="button"
-                style={{ marginLeft: '1vw', width: '7vw' }}
-                onClick={() => { this.solicitarLibro(libro.id) }}
-              >
-                Solicitar libro
-          </Button>}
-          </div>
-        </Paper> : null}
+        {this.state.visible ? (
+          <Paper className={rootClassName}>
+            <div className={classes.imageWrapper}>
+              <img
+                alt='Libro'
+                className={classes.image}
+                src='https://odcspress.org/wp-content/uploads/2017/01/Stack-Books-Copy.jpg'
+              />
+            </div>
+            <div className={classes.details}>
+              <Typography className={classes.title} variant='h4'>
+                {libro.titulo_libro}
+              </Typography>
+              <Typography className={classes.description} variant='body1'>
+                {libro.nombre_autor}
+              </Typography>
+            </div>
+            <Divider />
+            <div className={classes.stats}>
+              {/* <AccessTimeIcon className={classes.updateIcon} /> */}
+              <Typography className={classes.updateText} variant='body2'>
+                Genero: {libro.nombre_genero}
+              </Typography>
+              {/* <GetAppIcon className={classes.downloadsIcon} /> */}
+              <Typography className={classes.downloadsText} variant='body2'>
+                Idioma: {libro.significado_idioma}
+              </Typography>
+              {tipo == 'Administrador' ? (
+                <Button
+                  color='secondary'
+                  size='small'
+                  variant='outlined'
+                  type='button'
+                  style={{ marginLeft: '1vw', width: '7vw' }}
+                  onClick={() => {
+                    this.deleteLibro(libro.id);
+                  }}>
+                  Eliminar libro
+                </Button>
+              ) : (
+                <div>{this.mapButton(libro.id)}</div>
+              )}
+            </div>
+          </Paper>
+        ) : null}
       </div>
     );
   }
--- src/views/Solicitudes/components/SolicitudesToolbar/index.jsx
+++ src/views/Solicitudes/components/SolicitudesToolbar/index.jsx
@@ -26,7 +26,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import {
   MuiPickersUtilsProvider,
-  KeyboardDatePicker,
+  KeyboardDatePicker
 } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 
@@ -34,7 +34,6 @@ import DateFnsUtils from '@date-io/date-fns';
 import styles from './styles';
 
 class SolicitudesToolbar extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -46,26 +45,25 @@ class SolicitudesToolbar extends Component {
       fecha_prestamo: Date(),
       users: [],
       libros: []
-    }
-
+    };
   }
 
   handleClickOpen = () => {
-    this.setState({ open: true })
-  }
+    this.setState({ open: true });
+  };
 
   handleClose = () => {
-    this.setState({ open: false })
-  }
+    this.setState({ open: false });
+  };
 
   handleSubmit = () => {
-    let data = this.state
-    delete data['libros']
-    delete data['users']
-    delete data['open']
-    this.savePrestamo(data)
-    console.log(data)
-  }
+    let data = this.state;
+    delete data['libros'];
+    delete data['users'];
+    delete data['open'];
+    this.savePrestamo(data);
+    console.log(data);
+  };
 
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
@@ -76,23 +74,24 @@ class SolicitudesToolbar extends Component {
   }
 
   componentDidMount() {
-    this.getLibros()
-    this.getUsers()
+    this.getLibros();
+    this.getUsers();
   }
 
   savePrestamo(data) {
     try {
-      axios.post('https://cerinfo-api.herokuapp.com/prestamos', {
-        usuario_id: data.usuario_id,
-        libro_id: data.libro_id,
-        fecha_devolucion: data.fecha_devolucion,
-        fecha_prestamo: data.fecha_prestamo,
-        dias_prestados: data.dias_prestados
-      })
-        .then(res => {
-          console.log(res)
-          this.handleClose()
+      axios
+        .post('https://cerinfo-api.herokuapp.com/prestamos', {
+          usuario_id: data.usuario_id,
+          libro_id: data.libro_id,
+          fecha_devolucion: data.fecha_devolucion,
+          fecha_prestamo: data.fecha_prestamo,
+          dias_prestados: data.dias_prestados
         })
+        .then(res => {
+          console.log(res);
+          this.handleClose();
+        });
     } catch (error) {
       console.error(error);
     }
@@ -100,7 +99,9 @@ class SolicitudesToolbar extends Component {
 
   async getUsers() {
     try {
-      const response = await axios.get('https://cerinfo-api.herokuapp.com/usuarios');
+      const response = await axios.get(
+        'https://cerinfo-api.herokuapp.com/usuarios'
+      );
       this.setState({ users: response.data });
     } catch (error) {
       console.error(error);
@@ -109,7 +110,9 @@ class SolicitudesToolbar extends Component {
 
   async getLibros() {
     try {
-      const response = await axios.get('https://cerinfo-api.herokuapp.com/libros');
+      const response = await axios.get(
+        'https://cerinfo-api.herokuapp.com/libros'
+      );
       this.setState({ libros: response.data });
     } catch (error) {
       console.error(error);
@@ -140,86 +143,97 @@ class SolicitudesToolbar extends Component {
             className={classes.searchInput}
             placeholder="Buscar Libro"
           /> */}
-          {this.state.open ? <Dialog fullWidth={'xl'} open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title">Crear Prestamo</DialogTitle>
-            <DialogContent style={{ display: 'flex', flexDirection: 'column' }}>
-              <DialogContentText>
-                Crear nuevo prestamo
-            </DialogContentText>
-              <TextField
-                onChange={this.handleChange.bind(this)}
-                name={'dias_prestados'}
-                autoFocus
-                margin="dense"
-                id="dias_prestados"
-                label="Dias"
-                type="number"
-                fullWidth
-              />
-              <FormControl className={classes.formControl}>
-                <InputLabel htmlFor="usuario_id-simple">Usuario</InputLabel>
-                {this.state.users != [] ? <Select
-                  native
-                  fullWidth
-                  margin="dense"
-                  value={this.state.usuario_id}
+          {this.state.open ? (
+            <Dialog
+              fullWidth={'xl'}
+              open={this.state.open}
+              onClose={this.handleClose}
+              aria-labelledby='form-dialog-title'>
+              <DialogTitle id='form-dialog-title'>Crear Prestamo</DialogTitle>
+              <DialogContent
+                style={{ display: 'flex', flexDirection: 'column' }}>
+                <DialogContentText>Crear nuevo prestamo</DialogContentText>
+                <TextField
                   onChange={this.handleChange.bind(this)}
-                  inputProps={{
-                    name: 'usuario_id',
-                    id: 'usuario_id-simple',
-                  }}
-                >
-                  {this.state.users.map((user) =>
-                    <option value={user.id}>
-                      {user.nombre_usuario} - Reg: {user.registro_usuario}
-                    </option >
-                  )}
-                </Select> : null}
-              </FormControl>
-              <FormControl className={classes.formControl}>
-                <InputLabel htmlFor="libro_id-simple">Libro</InputLabel>
-                {this.state.libros != [] ? <Select
-                  native
+                  name={'dias_prestados'}
+                  autoFocus
+                  margin='dense'
+                  id='dias_prestados'
+                  label='Dias'
+                  type='number'
                   fullWidth
-                  margin="dense"
-                  value={this.state.libro_id}
-                  onChange={this.handleChange.bind(this)}
-                  inputProps={{
-                    name: 'libro_id',
-                    id: 'libro_id-simple',
-                  }}
-                >
-                  {this.state.libros.map((libro, index) =>
-                    <option value={libro.id}>
-                      {libro.titulo_libro}
-                    </option >
-                  )}
-                </Select> : null}
-              </FormControl>
-              <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <KeyboardDatePicker
-                  margin="normal"
-                  id="fecha_devolucion"
-                  label="Fecha de devolucion"
-                  format="dd/MM/yyyy"
-                  minDate={new Date()}
-                  value={this.state.fecha_devolucion}
-                  onChange={this.handleChangeDate.bind(this)}
-                  KeyboardButtonProps={{
-                    'aria-label': 'change date',
-                  }}
                 />
-              </MuiPickersUtilsProvider>
-            </DialogContent>
-            <DialogActions>
-              <Button type={'button'} onClick={this.handleClose} color="primary">
-                Cancelar
-          </Button>
-              <Button type={'button'} onClick={this.handleSubmit} color="primary">
-                Crear
-          </Button>
-            </DialogActions>
-          </Dialog> : null}
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor='usuario_id-simple'>Usuario</InputLabel>
+                  {this.state.users != [] ? (
+                    <Select
+                      native
+                      fullWidth
+                      margin='dense'
+                      value={this.state.usuario_id}
+                      onChange={this.handleChange.bind(this)}
+                      inputProps={{
+                        name: 'usuario_id',
+                        id: 'usuario_id-simple'
+                      }}>
+                      {this.state.users.map(user => (
+                        <option value={user.id}>
+                          {user.nombre_usuario} - Reg: {user.registro_usuario}
+                        </option>
+                      ))}
+                    </Select>
+                  ) : null}
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor='libro_id-simple'>Libro</InputLabel>
+                  {this.state.libros != [] ? (
+                    <Select
+                      native
+                      fullWidth
+                      margin='dense'
+                      value={this.state.libro_id}
+                      onChange={this.handleChange.bind(this)}
+                      inputProps={{
+                        name: 'libro_id',
+                        id: 'libro_id-simple'
+                      }}>
+                      {this.state.libros.map((libro, index) => (
+                        <option value={libro.id}>{libro.titulo_libro}</option>
+                      ))}
+                    </Select>
+                  ) : null}
+                </FormControl>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <KeyboardDatePicker
+                    margin='normal'
+                    id='fecha_devolucion'
+                    label='Fecha de devolucion'
+                    format='dd/MM/yyyy'
+                    minDate={new Date()}
+                    value={this.state.fecha_devolucion}
+                    onChange={this.handleChangeDate.bind(this)}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date'
+                    }}
+                  />
+                </MuiPickersUtilsProvider>
+              </DialogContent>
+              <DialogActions>
+                <Button
+                  type={'button'}
+                  onClick={this.handleClose}
+                  color='primary'>
+                  Cancelar
+                </Button>
+                <Button
+                  type={'button'}
+                  onClick={this.handleSubmit}
+                  color='primary'>
+                  Crear
+                </Button>
+              </DialogActions>
+            </Dialog>
+          ) : null}
           {/* <span className={classes.spacer} /> */}
           {/* <DisplayMode mode="grid" /> */}
         </div>
