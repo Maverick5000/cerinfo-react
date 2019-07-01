f378365f7f354fb4b07e143e5ee513a0bb3648a0
André Kraüss <andre.kmbv@gmail.com>
2019-06-30 23:04:57 -0400

    Charts

--- src/views/Multas/index.jsx
+++ src/views/Multas/index.jsx
@@ -14,10 +14,9 @@ import { Dashboard as DashboardLayout } from 'layouts';
 import { MultasToolbar } from './components';
 
 import axios from 'axios';
-
-import {
-  OrdersTable
-} from './components';
+import Chart from 'react-apexcharts';
+import { OrdersTable } from './components';
+import Paper from '@material-ui/core/Paper';
 
 // Component styles
 const styles = theme => ({
@@ -37,52 +36,110 @@ const styles = theme => ({
 });
 
 class Multas extends Component {
-
   state = {
-    nMultas: null
+    nMultas: null,
+    options: {
+      chart: {
+        id: 'basic-bar'
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    },
+    series: [
+      {
+        name: 'Multas',
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }
+    ]
   };
 
   reloadMultas = () => {
     const id = localStorage.getItem('id');
     const tipo = localStorage.getItem('tipo_usuario');
-    let url = ''
+    let url = '';
     if (tipo == 'Administrador') {
-      url = 'https://cerinfo-api.herokuapp.com/multas'
+      url = 'https://cerinfo-api.herokuapp.com/multas';
     } else {
-      url = 'https://cerinfo-api.herokuapp.com/user/multas'
+      url = 'https://cerinfo-api.herokuapp.com/user/multas';
     }
     //const id = '1'
 
-    axios.get(url, { params: { usuario_id: id } })
-      .then(res => {
-        this.setState({ nMultas: res.data });
-      })
-  }
+    axios.get(url, { params: { usuario_id: id } }).then(res => {
+      this.setState({ nMultas: res.data });
+      this.prepareData(res.data);
+    });
+  };
+
+  prepareData = data => {
+    let finalFechas = [];
+    let finalMonto = [];
+    let monto = 0;
+    data.forEach(multa => {
+      var myDate = new Date(multa.created_at);
+      monto = monto + parseInt(multa.monto_multa);
+      finalMonto.push(monto);
+      finalFechas.push(myDate.getDate());
+    });
+    this.setState({
+      options: {
+        chart: {
+          id: 'basic-bar'
+        },
+        xaxis: {
+          categories: finalFechas
+        },
+        title: {
+          text: 'Valor multas mensual',
+          align: 'left'
+        },
+        grid: {
+          row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5
+          }
+        }
+      },
+      series: [
+        {
+          name: 'Bolivianos',
+          data: finalMonto
+        }
+      ]
+    });
+  };
 
   componentDidMount() {
-    this.reloadMultas()
+    this.reloadMultas();
   }
 
   render() {
     const { classes } = this.props;
 
     return (
-      <DashboardLayout title="Multas">
+      <DashboardLayout title='Multas'>
         <div className={classes.root}>
           <MultasToolbar reload={this.reloadMultas} />
-          <Grid
-            container
-            justify="center"
-            spacing={4}
-          >
-            <Grid
-              item
-              lg={12}
-              md={16}
-              xl={13}
-              xs={16}
-            >
-              {this.state.nMultas ? <OrdersTable reload={this.reloadMultas} multas={this.state.nMultas} className={classes.item} /> : null}
+          <Grid container justify='center' spacing={4}>
+            <Grid item lg={12} md={16} xl={13} xs={16}>
+              {this.state.nMultas ? (
+                <OrdersTable
+                  reload={this.reloadMultas}
+                  multas={this.state.nMultas}
+                  className={classes.item}
+                />
+              ) : null}
+              {this.state.finalMonto != [] ? (
+                <Paper style={{ marginTop: '5vh' }} className={classes.root}>
+                  <Chart
+                    options={this.state.options}
+                    series={this.state.series}
+                    type='line'
+                    width='100%'
+                    height='350'
+                  />
+                </Paper>
+              ) : null}
             </Grid>
           </Grid>
         </div>
