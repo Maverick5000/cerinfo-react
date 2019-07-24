03502c9511e3c4a2ae876efa752370252f2aeb2c
Andre Krauss <andre.kmbv@gmail.com>
2019-07-24 17:24:01 -0400

    Deploy

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -21,82 +21,21 @@ export default class Routes extends Component {
   render() {
     return (
       <Switch>
-        <Redirect
-          exact
-          from="/"
-          to="/dashboard"
-        />
-        <Route
-          component={Dashboard}
-          exact
-          path="/dashboard"
-        />
-        <Route
-          component={UserList}
-          exact
-          path="/users"
-        />
-        <Route
-          component={ProductList}
-          exact
-          path="/libros"
-        />
-        <Route
-          component={Typography}
-          exact
-          path="/typography"
-        />
-        <Route
-          component={Icons}
-          exact
-          path="/icons"
-        />
-        <Route
-          component={Account}
-          exact
-          path="/account"
-        />
-        <Route
-          component={Settings}
-          exact
-          path="/settings"
-        />
-        <Route
-          component={SignUp}
-          exact
-          path="/sign-up"
-        />
-        <Route
-          component={SignIn}
-          exact
-          path="/sign-in"
-        />
-        <Route
-          component={UnderDevelopment}
-          exact
-          path="/under-development"
-        />
-        <Route
-          component={NotFound}
-          exact
-          path="/not-found"
-        />
-        <Route
-          component={Multas}
-          exact
-          path="/Multas"
-        />
-        <Route
-          component={Prestamos}
-          exact
-          path="/Prestamos"
-        />
-        <Route
-          component={Solicitudes}
-          exact
-          path="/Solicitudes"
-        />
-        <Redirect to="/dashboard" />
+        <Redirect exact from='/' to='/dashboard' />
+        <Route component={Dashboard} exact path='/dashboard' />
+        <Route component={UserList} exact path='/users' />
+        <Route component={ProductList} exact path='/libros' />
+        <Route component={Typography} exact path='/typography' />
+        <Route component={Icons} exact path='/icons' />
+        <Route component={Account} exact path='/account' />
+        <Route component={Settings} exact path='/settings' />
+        <Route component={SignUp} exact path='/sign-up' />
+        <Route component={SignIn} exact path='/sign-in' />
+        <Route component={UnderDevelopment} exact path='/under-development' />
+        <Route component={NotFound} exact path='/not-found' />
+        <Route component={Multas} exact path='/Multas' />
+        <Route component={Prestamos} exact path='/Prestamos' />
+        <Route component={Solicitudes} exact path='/Solicitudes' />
       </Switch>
     );
   }
